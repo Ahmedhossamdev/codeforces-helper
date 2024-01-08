742e0cf35c1111565af0239f84cade3b5cab03c3
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-01-08 04:26:54 +0200

    refactor(blogs): :fire: large refactor in vote

--- src/modules/problems/problems.controller.ts
+++ src/modules/problems/problems.controller.ts
@@ -1,22 +1,20 @@
-import {Body, Controller, Get, HttpCode, HttpStatus, Post, Query} from '@nestjs/common';
-import {ProblemsService} from "./problems.service";
-import {AddProblemDto} from "./dto";
-import {GetCurrentUser, GetCurrentUserId} from "../common/decorators";
-
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Query } from '@nestjs/common';
+import { ProblemsService } from './problems.service';
+import { AddProblemDto } from './dto';
+import { GetCurrentUser, GetCurrentUserId } from '../common/decorators';
 
 @Controller('problems')
 export class ProblemsController {
-    constructor(private readonly problemsService: ProblemsService) {}
+  constructor(private readonly problemsService: ProblemsService) {}
 
-    @Post('add')
-    @HttpCode(HttpStatus.OK)
-    async addProblem(@Body() dto : AddProblemDto , @GetCurrentUserId() userId : number){
-        return await this.problemsService.addProblem(dto , userId);
-    }
-    @Get('problems-by-tags')
-    async getProblemsByTags(@Query('tags') tags: string, @GetCurrentUserId() userId: number) {
-        const tagsArray = tags.split(',');
-        return await this.problemsService.getProblemsByTags(tagsArray, userId);
-    }
-
+  @Post('add')
+  @HttpCode(HttpStatus.OK)
+  async addProblem(@Body() dto: AddProblemDto, @GetCurrentUserId() userId: number) {
+    return await this.problemsService.addProblem(dto, userId);
+  }
+  @Get('problems-by-tags')
+  async getProblemsByTags(@Query('tags') tags: string, @GetCurrentUserId() userId: number) {
+    const tagsArray = tags.split(',');
+    return await this.problemsService.getProblemsByTags(tagsArray, userId);
+  }
 }
